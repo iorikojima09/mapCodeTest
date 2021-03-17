ed4d728ad729595ebea1686aa2781ac31499dde7
Iori Kojima <kojima.iori@gmail.com>
2021-03-17 12:05:35 +0900

    mapのピンを追加

--- mapToAR/ContentView.swift
+++ mapToAR/ContentView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 import MapKit
 
+//スポットの構造体
+struct Spot: Identifiable {
+    let id = UUID()
+    let latitude: Double
+    let longitude: Double
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
+
 struct ContentView: View {
-    
+    //指し示す座標
+    let spotlist = [
+        Spot(latitude: 35.683932, longitude: 139.764075),
+        Spot(latitude: 35.681038, longitude: 139.764568),
+        Spot(latitude: 35.681666, longitude: 139.770362)
+    ]
     //座標と領域を指定する
     @State var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(
@@ -19,10 +34,22 @@ struct ContentView: View {
         latitudinalMeters: 1000.0, //南北距離
         longitudinalMeters: 1000.0 //東西距離
     )
-    
     var body: some View {
-        Map(coordinateRegion: $region)
-            .edgesIgnoringSafeArea(.all)
+        Map(coordinateRegion: $region,
+            annotationItems: spotlist,
+            //annotationContent:{ spot in MapMarker(coordinate: spot.coordinate, tint: .orange)}
+            annotationContent:{ spot in
+                MapMarker(coordinate: spot.coordinate, tint: .orange)
+                //MAPピンを改良したもの
+                /*
+                MapAnnotation(coordinate: spot.coordinate,
+                              content: {
+                                Image(systemName: "circle.fill").foregroundColor(Color(red: 1.0, green: 0.0, blue: 128.0, opacity: 128.0))
+                              })
+                */
+            }
+        )
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -35,5 +62,5 @@ struct ContentView_Previews: PreviewProvider {
 
 
 /*
- 
+ testcode
 */
