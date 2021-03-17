c4937dec0d6b47a52d474f358891769d961e3252
Iori Kojima <kojima.iori@gmail.com>
2021-03-17 09:58:18 +0900

    Initial Commit

---
+++ mapToAR/mapToARApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapToARApp.swift
+//  mapToAR
+//
+//  Created by 小島伊織 on 2021/03/17.
+//
+
+import SwiftUI
+
+@main
+struct mapToARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
